743ca2c800c05e00d2d0c6bbe2b6d9b3dea0f9a9
azaadd <abulkalam.kta@gmail.com>
2023-07-05 14:53:05 +0600

    added register form in register page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -10,12 +10,12 @@ const Login = () => {
             <Form className='border p-5 rounded'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" name='password' placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Remember me" />
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className='container w-50 mx-auto'>
+            <h2 className='text-center fw-bold mb-3'>Please Register</h2>
+            <Form className='border p-5 rounded'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter your name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Photo url</Form.Label>
+                    <Form.Control type="text" name='photo' placeholder="Enter your photo url" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Accept terms & conditions" />
+                </Form.Group>
+                <Button className='w-100 apply fw-semibold' variant="primary" type="submit">
+                    Register
+                </Button>
+                <p className='mt-2'>Already have an account? <Link to='/login'>Login</Link></p>
+            </Form>
+
         </div>
     );
 };
