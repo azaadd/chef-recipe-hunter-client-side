3b1ad68b936ef70ca1bff12ddc68b5796859f0f1
azaadd <abulkalam.kta@gmail.com>
2023-07-08 10:57:48 +0600

    added media query for mobile device

--- src/pages/Home/Home/Banners.js
+++ src/pages/Home/Home/Banners.js
@@ -6,14 +6,14 @@ import Chefs from './Chefs';
 const Banners = () => {
     return (
         <div className='container mt-5'>
-            <div className='d-flex align-items-center'>
-                <div>
+            <div className='align-items-center d-md-flex'>
+                <div className=''>
                     <h2 className='fw-bold fs-1 mb-4 text-success'>Bangladeshi <span className='text-danger'>cuisine is a</span> culinary art-form</h2>
                     <p className='fs-5'>A taste tantalizing blend of wonderful and fragrant spices that will keep you coming back for more. Bangladeshi culinary habits were strongly influenced by the cuisine of the area's Mughal rulers. This includes rich, aromatic dishes such as biriyani and korma that require the use of a large array of spices along with a great deal of ghee.</p>
                     <button type="button" className="apply mt-3">Order Now</button>
                 </div>
-                <div>
-                    <img src={banner} alt="" />
+                <div className=''>
+                    <img className='img-banner' src={banner} alt="" />
                 </div>
             </div>
             <Chefs></Chefs>
